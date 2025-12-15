11852d30e88a0f00f3d6b21a351c0d7e73b3c98d
Marwa <you@example.com>
2025-12-15 15:51:02 -0500

    env final

--- src/common/constants/ENV.ts
+++ src/common/constants/ENV.ts
@@ -2,11 +2,6 @@ import jetEnv, { num, str } from 'jet-env';
 import { isEnumVal } from 'jet-validators';
 import { NodeEnvs } from '.';
 
-console.log('Node:', process.env.NODE_ENV);
-console.log('Port:', process.env.PORT);
-console.log('Mongo:', process.env.MONGODB);
-console.log('JWT:', process.env.JWTSECRET);
-
 const ENV = jetEnv({
   NodeEnv: isEnumVal(NodeEnvs),
   Port: num,
