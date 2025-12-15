4e08842c882ffa728052df16bf4eb372f7b6b3ee
Marwa <you@example.com>
2025-12-15 07:37:52 -0500

    version finale

--- dist/models/Chat.js
+++ dist/models/Chat.js
@@ -1,23 +1,42 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
 exports.Chat = void 0;
-const mongoose_1 = require("mongoose");
-const ChatSchema = new mongoose_1.Schema({
-    nom: { type: String, required: true },
-    race: { type: String, required: true },
-    numeroDossier: { type: Number, required: true, unique: true },
-    dateNaissance: { type: Date, required: true },
-    tauxEnergie: { type: Number, min: 1, max: 5, required: true },
-    sociabiliteHumain: { type: Number, min: 1, max: 5, required: true },
-    compatEnfants: { type: Number, min: 1, max: 5, required: true },
-    compatChiens: { type: Number, min: 1, max: 5, required: true },
-    compatChats: { type: Number, min: 1, max: 5, required: true },
-    description: { type: String, required: true },
-    micropuce: { type: Boolean, required: true },
-    sterilise: { type: Boolean, required: true },
-    vaccinsBase: { type: Boolean, required: true },
-    photos: { type: [String], default: [] },
-    created: { type: Date, default: Date.now },
+const mongoose_1 = require('mongoose');
+const ChatSchema = new Schema({
+  nom: { type: String, required: true },
+  race: { type: String, required: true },
+  sexe: { type: String, enum: ['Femelle', 'Mâle'], required: true },
+
+  poids: { type: Number, required: true },
+
+  numeroDossier: { type: Number, required: true, unique: true },
+
+  dateNaissance: { type: Date, required: true },
+  dateMiseAdoption: { type: Date, required: true },
+
+  tauxEnergie: { type: Number, min: 1, max: 5, required: true },
+  sociabiliteHumain: { type: Number, min: 1, max: 5, required: true },
+  compatEnfants: { type: Number, min: 1, max: 5, required: true },
+  compatChiens: { type: Number, min: 1, max: 5, required: true },
+  compatChats: { type: Number, min: 1, max: 5, required: true },
+
+  description: { type: String, required: true },
+
+  micropuce: { type: Boolean, required: true },
+  sterilise: { type: Boolean, required: true },
+  vaccinsBase: { type: Boolean, required: true },
+  disponible: { type: Boolean, default: true },
+
+  coutTotal: { type: Number, required: true },
+  coutSterilisation: { type: Number, required: true },
+  coutVaccin: { type: Number, required: true },
+  coutVermifuge: { type: Number, required: true },
+  coutMicropuce: { type: Number, required: true },
+
+  photos: { type: [String], default: [] },
+
+  created: { type: Date, default: Date.now },
 });
+
 exports.Chat = (0, mongoose_1.model)('Chat', ChatSchema);
 exports.default = exports.Chat;
