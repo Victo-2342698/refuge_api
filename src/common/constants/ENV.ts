08f823e2ca880bd1ff59708969521298096e268b
Marwa <you@example.com>
2025-12-15 14:49:24 -0500

    console log5

--- src/common/constants/ENV.ts
+++ src/common/constants/ENV.ts
@@ -2,19 +2,11 @@ import jetEnv, { num, str } from 'jet-env';
 import { isEnumVal } from 'jet-validators';
 import { NodeEnvs } from '.';
 
-/******************************************************************************
-                                 Setup
-******************************************************************************/
-
 const ENV = jetEnv({
-  NodeEnv: isEnumVal(NodeEnvs),
-  Port: num,
-  Mongodb: str,
-  Jwtsecret: str,
+  NODE_ENV: isEnumVal(NodeEnvs),
+  PORT: num,
+  MONGODB: str,
+  JWTSECRET: str,
 });
-
-/******************************************************************************
-                            Export default
-******************************************************************************/
 
 export default ENV;
